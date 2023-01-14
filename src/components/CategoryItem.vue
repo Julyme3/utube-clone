0bef5716e91a37c13fcc9dc303f09ba8f6e22a79
Julyme3 <jg0311@bk.ru>
2023-01-14 21:25:58 +0500

    add markup for category item component

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <a href="#" :class="classes">{{ category }}</a>
+</template>
+
+<script>
+export default {
+  name: 'CategoryItem',
+  props: ['category', 'isActive'],
+  computed: {
+    classes() {
+      const classes = ['px-3', 'py-1', 'transition', 'bg-gray-600', 'border'];
+
+      return this.isActive
+        ? [
+            ...classes,
+            'border-gray-700',
+            'rounded-full',
+            'hover:bg-gray-500',
+            'text-white',
+          ]
+        : [...classes, 'border-gray-300', 'bg-gray-100', 'hover:bg-gray-200'];
+    },
+  },
+};
+</script>
+
+<style scoped></style>
