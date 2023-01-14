6be8e4e3ebd6aeb4c9cf952cd85f1a4fc9ccba40
Julyme3 <jg0311@bk.ru>
2023-01-14 22:29:21 +0500

    add markup for header component

---
+++ src/components/TheSearch.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex w-full h-full">
+    <TheSearchInput />
+    <TheSearchButton />
+  </div>
+</template>
+
+<script>
+import TheSearchInput from './TheSearchInput.vue';
+import TheSearchButton from './TheSearchButton.vue';
+export default {
+  name: 'TheSearch',
+  components: { TheSearchButton, TheSearchInput },
+};
+</script>
+
+<style scoped></style>
