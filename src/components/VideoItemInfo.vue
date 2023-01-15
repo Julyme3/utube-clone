c76fb386604adbaeb73554bd6fd4526604071685
Julyme3 <jg0311@bk.ru>
2023-01-15 12:39:05 +0500

    fix summary in VideoItemInfo

--- src/components/VideoItemInfo.vue
+++ src/components/VideoItemInfo.vue
@@ -11,11 +11,7 @@
         <span>Channel name</span>
         <BaseIcon name="checkCircle" class="w-3.5 h-3.5 ml-1" />
       </div>
-      <div>
-        <span>1K views</span>
-        &middot;
-        <span>1 day ago</span>
-      </div>
+      <div v-html="summary"></div>
     </div>
     <button
       class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none"
@@ -31,6 +27,12 @@ export default {
   name: 'VideoItemInfo',
   components: { BaseIcon },
   props: ['index'],
+  computed: {
+    summary() {
+      const days = this.index === 1 ? 'day' : 'days';
+      return `1K views &middot; ${this.index} ${days} ago`;
+    },
+  },
 };
 </script>
 
