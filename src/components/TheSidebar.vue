b86a6639110b285de96a32326722b6ad1305763c
Julyme3 <jg0311@bk.ru>
2023-01-15 16:50:50 +0500

    add SidebarSection component

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -2,45 +2,28 @@
   <aside
     class="hidden xl:block w-64 max-h-screen overflow-auto fixed top-0 pt-12 bg-white z-20"
   >
-    <section class="py-2 border-b">
+    <SidebarSection>
       <ul>
         <SidebarNavItem label="Home" icon="home" isActive />
         <SidebarNavItem label="Trending" icon="fire" />
         <SidebarNavItem label="Subscriptions" icon="subscriptions" />
       </ul>
-    </section>
-    <section class="py-2 border-b">
+    </SidebarSection>
+    <SidebarSection>
       <ul>
         <SidebarNavItem label="Library" icon="duplicate" />
-        <SidebarNavItem label="History" icon="time" />
-      </ul>
-    </section>
-    <section class="px-8 py-4 font-medium leading-5 border-b">
+        <SidebarNavItem label="History" icon="time" /></ul
+    ></SidebarSection>
+    <SidebarSection class="px-8 py-4 font-medium leading-5 border-b">
       <span class="text-sm">
         Sign in to like videos, comment, and subscribe.
       </span>
-      <button
+      <ButtonLogin
         class="focus:outline-none flex items-center px-2 py-0.5 mt-3 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm"
-      >
-        <svg
-          class="mr-2 w-7 h-7"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-        Sign In
-      </button>
-    </section>
-    <section class="py-2 border-b">
-      <div class="uppercase text-sm font-semibold px-6 py-2">
-        Best of YouTube
-      </div>
+      />
+    </SidebarSection>
+    <SidebarSection>
+      <SidebarSectionHeading title="Best of YouTube" />
       <ul>
         <SidebarNavItem label="Music" icon="music" />
         <SidebarNavItem label="Sports" icon="sport" />
@@ -50,56 +33,46 @@
         <SidebarNavItem label="Live" icon="live" />
         <SidebarNavItem label="360&deg; Video" icon="video360" />
       </ul>
-    </section>
-    <section class="py-2 border-b">
+    </SidebarSection>
+    <SidebarSection>
       <ul>
         <SidebarNavItem label="Browse channels" icon="channels" />
       </ul>
-    </section>
-    <section class="py-2 border-b">
-      <div class="uppercase text-sm font-semibold px-6 py-2">
-        More from YouTube
-      </div>
+    </SidebarSection>
+    <SidebarSection>
+      <SidebarSectionHeading title="More from YouTube" />
       <ul>
         <SidebarNavItem label="YouTube Premium" icon="premium" />
         <SidebarNavItem label="Live" icon="live" />
       </ul>
-    </section>
-    <section class="py-2 border-b">
+    </SidebarSection>
+    <SidebarSection>
       <ul>
         <SidebarNavItem label="Settings" icon="settings" />
         <SidebarNavItem label="Report history" icon="report" />
         <SidebarNavItem label="Help" icon="questionMark" />
         <SidebarNavItem label="Send feedback" icon="message" />
       </ul>
-    </section>
-    <section class="px-6 py-4 text-xs font-semibold space-y-4">
-      <div>
-        <a href="#" class="mr-2">About</a>
-        <a href="#" class="mr-2">Press</a>
-        <a href="#" class="mr-2">Copyright</a>
-        <a href="#" class="mr-2">Contact us</a>
-        <a href="#" class="mr-2">Creators</a>
-        <a href="#" class="mr-2">Advertise</a>
-        <a href="#" class="mr-2">Developers</a>
-      </div>
-      <div>
-        <a href="#" class="mr-2">Terms</a>
-        <a href="#" class="mr-2">Privacy</a>
-        <a href="#" class="mr-2">Policy & Safety</a>
-        <a href="#" class="mr-2">How YouTube works</a>
-        <a href="#" class="mr-2">Test new features</a>
-      </div>
-      <div class="font-normal text-gray-500">&copy; 2021 Google LLC</div>
-    </section>
+    </SidebarSection>
+    <SidebarSection class="px-6 py-4 text-xs font-semibold space-y-4">
+      <TheFooter />
+    </SidebarSection>
   </aside>
 </template>
 
 <script>
 import SidebarNavItem from './SidebarNavItem.vue';
+import ButtonLogin from './ButtonLogin.vue';
+import SidebarSectionHeading from './SidebarSectionHeading.vue';
+import SidebarSection from './SidebarSection.vue';
+import TheFooter from './TheFooter.vue';
 export default {
   name: 'TheSidebar',
   components: {
+    TheFooter,
+    SidebarSection,
+    SidebarSectionHeading,
+    ButtonLogin,
     SidebarNavItem,
   },
 };
