0a9c56e4e8cf603e8a78ea7fba3a806a9054489c
Julyme3 <jg0311@bk.ru>
2023-01-16 16:02:46 +0500

    add animation for show/hide sidebar

--- src/components/TheSidebarMobile.vue
+++ src/components/TheSidebarMobile.vue
@@ -1,15 +1,35 @@
 <template>
-  <SidebarOverlay>
-    <aside class="w-64 max-h-screen overflow-auto bg-white">
+  <transition
+    enter-active-class="transition-opacity ease-linear duration-200"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition-opacity ease-linear duration-200"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+    ><SidebarOverlay v-show="isOpen" @click="isOpen = false"></SidebarOverlay
+  ></transition>
+
+  <transition
+    enter-active-class="transition ease-in-out duration-200 transform"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="transition ease-in-out duration-200 transform"
+    leave-from-class="translate-x-0"
+    leave-to-class="-translate-x-full"
+  >
+    <aside
+      v-show="isOpen"
+      class="w-64 max-h-screen overflow-auto bg-white fixed z-40"
+    >
       <section class="flex items-center p-4 border-b sticky top-0 bg-white">
-        <button class="ml-2 mr-6 focus:outline-none">
+        <button @click="isOpen = false" class="ml-2 mr-6 focus:outline-none">
           <BaseIcon name="menu" />
         </button>
         <LogoMain />
       </section>
       <SidebarContent />
     </aside>
-  </SidebarOverlay>
+  </transition>
 </template>
 
 <script>
@@ -20,6 +40,11 @@ import LogoMain from './LogoMain.vue';
 export default {
   name: 'TheSidebarMobile',
   components: { LogoMain, BaseIcon, SidebarContent, SidebarOverlay },
+  data() {
+    return {
+      isOpen: true,
+    };
+  },
 };
 </script>
 
