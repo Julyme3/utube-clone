23cd99f2d1dd354850a4014254ab6411fc419d9c
Julyme3 <jg0311@bk.ru>
2023-01-31 13:22:20 +0500

    add TheSearchResults component

--- src/components/TheSearch.vue
+++ src/components/TheSearch.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex w-full mr-2">
-    <TheSearchInput />
+    <div class="relative flex w-full">
+      <TheSearchInput />
+      <TheSearchResults :results="keywords" />
+    </div>
     <TheSearchButton />
   </div>
 </template>
@@ -8,9 +11,30 @@
 <script>
 import TheSearchInput from './TheSearchInput.vue';
 import TheSearchButton from './TheSearchButton.vue';
+import TheSearchResults from './TheSearchResults.vue';
 export default {
   name: 'TheSearch',
-  components: { TheSearchButton, TheSearchInput },
+  components: { TheSearchResults, TheSearchButton, TheSearchInput },
+  data() {
+    return {
+      keywords: [
+        'new york giants',
+        'new york alicia keys',
+        'new york giants vs washington football',
+        'new york',
+        'new york song',
+        'new york new york frank sinatra',
+        'new york jets',
+        'new york city',
+        'new york giants live',
+        'new york state of mind',
+        'new york giants vs washington football live',
+        'new york giants injury',
+        'new york giants live stream',
+        'new york accent',
+      ],
+    };
+  },
 };
 </script>
 
