6dd63ff1c6f2b879abc13d8da518b67a21ea1651
Julyme3 <jg0311@bk.ru>
2023-02-06 12:17:01 +0500

    add TheSearchPredictionsList component

--- src/components/TheModalSearchPredictions.vue
+++ src/components/TheModalSearchPredictions.vue
@@ -2,22 +2,10 @@
   <BaseModal class="md:w-4/5 max-w-2xl">
     <p class="text-base text-black tracking-wide">Report search predictions</p>
     <div class="space-y-3 text-black text-sm mt-5">
-      <div
-        v-for="(label, index) in searchPredictions"
-        :key="label"
-        class="flex items-center"
-      >
-        <input
-          type="checkbox"
-          class="h-5 w-5 cursor-pointer"
-          :id="index"
-          :value="label"
-          v-model="selectedSearchPredictions"
-        />
-        <label :for="index" class="pl-4 cursor-pointer flex-grow">
-          {{ label }}
-        </label>
-      </div>
+      <TheSearchPredictionsList
+        :search-predictions="searchPredictions"
+        v-model="selectedSearchPredictions"
+      />
     </div>
     <template #footer="slotProps">
       <button
@@ -38,9 +26,11 @@
 
 <script>
 import BaseModal from './BaseModal.vue';
+import TheSearchPredictionsList from './TheSearchPredictionsList.vue';
 export default {
   name: 'TheModalSearchPredictions',
   components: {
+    TheSearchPredictionsList,
     BaseModal,
   },
   data() {
---
+++ src/components/TheSearchPredictionsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    v-for="(label, index) in searchPredictions"
+    :key="label"
+    class="flex items-center"
+  >
+    <input
+      type="checkbox"
+      class="h-5 w-5 cursor-pointer"
+      :id="index"
+      :value="label"
+      v-model="selectedSearchPredictions"
+    />
+    <label :for="index" class="pl-4 cursor-pointer flex-grow">
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheSearchPredictionsList',
+  props: {
+    searchPredictions: Array,
+    modelValue: Array,
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      selectedSearchPredictions: this.modelValue,
+    };
+  },
+  watch: {
+    selectedSearchPredictions() {
+      this.$emit('update:modelValue', this.selectedSearchPredictions);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
